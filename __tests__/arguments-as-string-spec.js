573a28600875baa0cc8b1db8f2306c047a8af68e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-04-05 10:26:34 -0400

    feat: tested several situations

--- __tests__/arguments-as-string-spec.js
+++ __tests__/arguments-as-string-spec.js
@@ -7,4 +7,40 @@ describe('arguments-as-string', () => {
   it('is a function', () => {
     expect(typeof argumentsAsString).toBe('function')
   })
+
+  it('forms a string from strings and numbers', () => {
+    expect(argumentsAsString('foo', 'bar', 42, 'the last argument')).toMatchSnapshot()
+  })
+
+  it('forms string from objects', () => {
+    const o = {
+      foo: {
+        bar: 42
+      }
+    }
+    const p = {
+      name: 'Joe'
+    }
+    const result = argumentsAsString(o, p)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('handles circular references', () => {
+    const o = {
+      foo: {
+        bar: 42
+      }
+    }
+    const p = {
+      name: 'Joe',
+    }
+    p.ref = p
+    const result = argumentsAsString(o, p)
+    expect(result).toMatchSnapshot()
+  })
+
+  it('handles numbers', () => {
+    const result = argumentsAsString(1, 2, 3, 4, 5, 6)
+    expect(result).toMatchSnapshot()
+  })
 })
